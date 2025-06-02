38dc53b7d14f1b67e0fea445365444cba1998012
HadesSoleil0462 <90576992+HadesSoleil0462@users.noreply.github.com>
2025-06-02 17:57:55 +0200

    Add user authorization middleware and implement bearer token authentication

--- narraty-web-api/src/main/java/com/ynov/javaformation/narraty/middlewares/BearerAuthenticationFilter.java
+++ narraty-web-api/src/main/java/com/ynov/javaformation/narraty/middlewares/BearerAuthenticationFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -24,8 +23,8 @@ public class BearerAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    @NotNull HttpServletResponse response,
-                                    @NotNull FilterChain filterChain)
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
             throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
